2e273579ca573da745d9d6a9ab92441e7b1b2d87
prince125 <54930606+prince125@users.noreply.github.com>
2019-10-08 16:59:10 +0530

    1 commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+ export const environment= {
+   production: true,
+   firebase: {
+  apiKey: "AIzaSyAYlJoQtIUozEEOgtygTaI8tACoCYpgGNI",
+  authDomain: "ionic-4-firebase-login-d0e2c.firebaseapp.com",
+  databaseURL: "https://ionic-4-firebase-login-d0e2c.firebaseio.com",
+  projectId: "ionic-4-firebase-login-d0e2c",
+  storageBucket: "",
+  messagingSenderId: "45966833290",
+  appId: "1:45966833290:web:b75ced3a7a1bd50de71f73"
+   }
+};
